5ff0f404f80240a4c649492acb89cc1e1d7c4112
sinasio <synasius@gmail.com>
2024-05-15 23:01:32 +0200

    Addressed a couple of issues

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,7 +3,7 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	print("You died!")
 	timer.start()
 
