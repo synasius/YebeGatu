ab82a6f6f38a182685275be233cffba8dbe87243
sinasio <synasius@gmail.com>
2024-05-19 09:22:11 +0200

    Added a basic enemy

---
+++ scenes/mushroom.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+const SPEED = 60
+
+var direction = 1
+
+func _process(delta: float) -> void:
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+
+	position.x += direction * SPEED * delta
